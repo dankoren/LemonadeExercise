1822a9d9e23de6d3f987371a9eae83e17eab9fa3
dankoren <dankoren2@gmail.com>
2021-10-04 19:44:19 +0300

    Add files via upload

--- Services/wordsService.js
+++ Services/wordsService.js
@@ -8,22 +8,22 @@ module.exports = class WordsService{
     wordsCounter = {};
 
     constructor() {
-        this.loadFile(filePath);
-    }
+        loadFile(filePath);
 
-    loadFile = (filePath) => {
-        if(!fs.existsSync(filePath)){
-            fs.writeFileSync(filePath, '');
-        }
-        else{
-            const data = fs.readFileSync(filePath);
-            const plainText = data.toString();
-            if(plainText){
-                this.wordsCounter = JSON.parse(plainText);
+        const loadFile = (filePath) => {
+            if(!fs.existsSync(filePath)){
+                fs.writeFileSync(filePath, '');
+            }
+            else{
+                const data = fs.readFileSync(filePath);
+                const plainText = data.toString();
+                if(plainText){
+                    this.wordsCounter = JSON.parse(plainText);
+                }
             }
         }
     }
-    
+
     addWords = (text) =>{
         const extractWords = (text) => {
             const normalizedText = text.toLowerCase()
@@ -49,71 +49,10 @@ module.exports = class WordsService{
 
         const words = extractWords(text);
         words.forEach(word=> addWord(word));
-        saveToFile();
+        saveToFile(); // Making the data persistence
     }
 
     getWordStatistics = (word) => {
-        console.log("getWords started, word: " + word);
         return this.wordsCounter[word];
     }
-
-    // extractWords = (text) => {
-    //     foo();
-    //     const normalizedText = text.toLowerCase()
-    //                             .replace('-','')
-    //                             .replace(/\d+/g, '')
-    //                             .replace(',','');
-    //     console.log(normalizedText);
-    //     return normalizedText.split(' ');
-    // }
-
-// module.exports = {addWords}
-
-    // addWord = (word) => {
-    //     if(this.wordsCounter[word]){
-    //         this.wordsCounter[word]++;
-    //     }
-    //     else{
-    //         this.wordsCounter[word] = 1;
-    //     }
-    // }
-
-    // saveToFile = () => {
-    //     const data = JSON.stringify(this.wordsCounter);
-    //     console.log('data: ' + data);
-    //     fs.writeFile(filePath, data, (error) => {
-    //         if (error) throw error;
-    //     });
-    // }
 }
-
-// function extractWords(text){
-//     const normalizedText = text.toLowerCase()
-//     .replace('-','')
-//     .replace(/\d+/g, '')
-//     .replace(',','');
-//     console.log(normalizedText);
-//     return normalizedText.split(' ');
-// }
-
-
-
-// function getInstance()
-
-// function addWords(text){
-//     const words = extractWords(text);
-
-//     console.log(words);
-    
-// }
-
-// function extractWords(text){
-//     const normalizedText = text.toLowerCase()
-//                             .replace('-','')
-//                             .replace(/\d+/g, '')
-//                             .replace(',','');
-//     console.log(normalizedText);
-//     return normalizedText.split(' ');
-// }
-
-// module.exports = {addWords}
